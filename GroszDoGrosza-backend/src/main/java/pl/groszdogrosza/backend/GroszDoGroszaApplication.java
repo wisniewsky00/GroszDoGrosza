ce266291336f7d3cbb6373b9c7b08192972a2e62
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2025-11-12 10:27:21 +0100

    Add initial backend structure

---
+++ GroszDoGrosza-backend/src/main/java/pl/groszdogrosza/backend/GroszDoGroszaApplication.java
@@ -0,0 +1,13 @@
+package pl.groszdogrosza.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroszDoGroszaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroszDoGroszaApplication.class, args);
+	}
+
+}
---
+++ GroszDoGrosza-backend/src/main/java/pl/groszdogrosza/backend/TestController.java
@@ -0,0 +1,14 @@
+package pl.groszdogrosza.backend;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+  
+  @GetMapping
+  public String hello() {
+    return "Backend works!";
+  }
+}
