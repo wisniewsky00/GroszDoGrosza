26b8cbd3c040490b05282e76b9a75e8a14e3c6f4
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2025-11-29 17:10:18 +0100

    feat: add mobile menu and change logo

--- GroszDoGrosza-frontend/src/components/Header.jsx
+++ GroszDoGrosza-frontend/src/components/Header.jsx
@@ -1,39 +1,75 @@
-import { NavLink, useNavigate } from 'react-router'
-import Logo from '../assets/images/GroszDoGrosza-Logo.png';
+import { NavLink, useNavigate } from 'react-router';
+import Logo from '../assets/images/GroszDoGrosza-Logo.svg';
 import './Header.css';
+import { useEffect, useState } from 'react';
 
 export function Header() {
-
   const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1200) {
+        setMenuOpen(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [])
 
   return (
-    <div className="header">
+    <header className="header">
       <div className="left-section">
         <NavLink to="/">
-          <img className="logo" src={Logo} />
-        </NavLink>  
+          <img className="logo" src={Logo} alt="GroszDoGrosza logo" />
+        </NavLink>
       </div>
 
-      <div className="middle-section">
-        <p>Nasza misja</p>
-        <p>Funkcje</p>
-        <p>Kontakt</p>
-      </div>
+      <nav className="middle-section">
+        <NavLink to="/#mission">Nasza misja</NavLink>
+        <NavLink to="/#features">Funkcje</NavLink>
+        <NavLink to="/#contact">Kontakt</NavLink>
+      </nav>
 
       <div className="right-section">
         <button 
           className="btn btn-green"
           onClick={() => navigate("/login")}
-        >
-          Logowanie
-        </button>
+        >Logowanie</button>
+
         <button 
           className="btn btn-blue"
           onClick={() => navigate("/register")}
-        >
-          Rejestracja
-        </button>  
+        >Rejestracja</button>
       </div>
-    </div>
+
+      <div
+        className={`burger ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+
+      <div
+        className={`mobile-menu ${menuOpen ? "show" : ""}`}
+      >
+        <NavLink onClick={() => setMenuOpen(false)} to="/#mission">Nasza misja</NavLink>
+        <NavLink onClick={() => setMenuOpen(false)} to="/#features">Funkcje</NavLink>
+        <NavLink onClick={() => setMenuOpen(false)} to="/#contact">Kontakt</NavLink>
+
+        <button onClick={() => {navigate("/login"); setMenuOpen(false)}} className="btn btn-green">
+          Logowanie
+        </button>
+
+        <button onClick={() => {navigate("/register"); setMenuOpen(false)}} className="btn btn-blue">
+          Rejestracja
+        </button>
+
+      </div>
+    </header>
   );
 }
