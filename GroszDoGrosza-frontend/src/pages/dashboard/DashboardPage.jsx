5fbecdf7d48b6f0f4c659e6f3ca070560f219f8d
wisniewsky00 <krystian.wisniewski2906@gmail.com>
2025-12-08 09:55:31 +0100

    feat: add dropdown and style dashboard header

--- GroszDoGrosza-frontend/src/pages/dashboard/DashboardPage.jsx
+++ GroszDoGrosza-frontend/src/pages/dashboard/DashboardPage.jsx
@@ -1,14 +1,13 @@
-import { useEffect, useState } from "react";
-import { useAuth} from "../../auth/useAuth";
+import { useEffect} from "react";
+import { useAuth } from "../../auth/useAuth";
 import { useNavigate } from 'react-router-dom';
 import { isTokenExpired } from "../../utils/jwtUtils";
-import { backendApi } from "../../services/backendApi";
 import { DashboardHeader } from "./DashboarHeader";
+import './DashboardPage.css';
 
 export function DashboardPage() {
-  
-  const {user, token, logout} = useAuth();
-  const [message, setMessage] = useState("");
+
+  const { user, token, logout } = useAuth();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,40 +26,9 @@ export function DashboardPage() {
     return () => clearInterval(interval);
   }, [token, logout, navigate]);
 
-  const handleTestApi = async () => {
-    try {
-      const response = await backendApi.get("/users/test");
-      setMessage(response.data);
-    } catch (error) {
-      console.log(error);
-      setMessage("Błąd dostępu lub token wygasł"); 
-    }
-  }
-
-  function handleLogout() {
-    logout();
-  }
-
   return (
     <>
-      <DashboardHeader />
-
-      <h1>Dashboard</h1>
-      <h2> Dane użytownika</h2>
-
-      {user && (
-        <div>
-          <p>ID: {user.id}</p>
-          <p>Username: {user.username}</p>
-          <p>Email: {user.email}</p>
-          <p>Role: {user.role}</p>
-          <p>Token: {token}</p>
-          <button onClick={handleTestApi}>Sprawdź API USER</button>
-        </div>
-      )}
-      {message && <p>Odpowiedź API: {message}</p>}
-
-      <button onClick={handleLogout}>Wyloguj</button>
+      <DashboardHeader user={user} logout={logout}/>
     </>
   );
 }
